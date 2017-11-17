29cae23bb71fbe3ca6b67a8d39e5bb442afeae6f
Adam Johnson <adamjohnson@Adams-MacBook-Pro.local>
2017-11-17 10:58:15 -0600

    add gem/require rails-controller-testing

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ require "action_cable/engine"
 require "sprockets/railtie"
 require "faker"
 require "rspec-rails"
+require "rails-controller-testing"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
